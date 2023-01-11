9cd96a4eaa8b0227e672c5ee50a8e259c56e9175
Maxence <verger.maxence42@gmail.com>
2023-01-11 17:39:42 +0100

    first launch

---
+++ src/include/cube.h
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cube.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mverger <mverger@42lyon.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/11 15:05:30 by mverger           #+#    #+#             */
+/*   Updated: 2023/01/11 17:16:25 by mverger          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUBE_H
+# define CUBE_H
+
+# include "../../mlx-linux/mlx.h"
+# include <math.h>
+
+# define screenWidth 800
+# define screenHeight 600
+
+typedef struct s_data
+{
+	int		**worldmap;
+	double	posx; //x and y start position
+	double	posy;
+	double	dirx; //initial direction vector
+	double	diry;
+	double	planex; //the 2d raycaster version of camera plane
+	double	planey;
+	double	camerax;
+	double	raydirx;
+	double	raydiry;
+	
+	double	time; //time of current frame
+	double	oldtime; //time of previous frame
+
+	int		mapx; //which box of the map we're in
+	int		mapy;
+	double	sidedistx; //length of ray from current position to next x or y-side
+	double	sidedisty;
+	double	deltadistx; //length of ray from one x or y-side to next x or y-side
+	double	deltadisty;
+	double	perpwalldist;
+	int		stepx; //what direction to step in x or y-direction (either +1 or -1)
+	int		stepy;
+	int		hit; //was there a wall hit?
+	int		side; //was a NS or a EW wall hit?
+	
+	void	*mlx;
+	void	*mlx_win;
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+	
+}			t_data;
+
+#endif
--- src/main.c
+++ src/main.c
@@ -3,11 +3,180 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jthibaul <jthibaul@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mverger <mverger@42lyon.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 16:41:53 by jthibaul          #+#    #+#             */
-/*   Updated: 2022/12/30 16:42:22 by jthibaul         ###   ########.fr       */
+/*   Updated: 2023/01/11 17:39:10 by mverger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/cube.h"
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void	calculate_step_init_sidedist(t_data *data)
+{
+	if (data->raydirx < 0)
+	{
+		data->stepx = -1;
+		data->sidedistx = (data->posx - data-> mapx) * data->deltadistx;
+	}
+	else
+	{
+		data->stepx = 1;
+		data->sidedistx = (data->mapx + 1.0 - data->posx) * data->deltadistx;
+	}
+	if (data->raydiry < 0)
+	{
+		data->stepy = -1;
+		data->sidedisty = (data->posy - data-> mapy) * data->deltadisty;
+	}
+	else
+	{
+		data->stepy = 1;
+		data->sidedisty = (data->mapy + 1.0 - data->posy) * data->deltadisty;
+	}
+	
+}
+
+void	ray_calculation(t_data *data)
+{
+	data->mapx = (int)(data->posx); //which box of the map we're in
+	data->mapy = (int)(data->posy);
+	data->sidedistx = 0; //length of ray from current position to next x or y-side
+	data->sidedisty = 0;
+	if (data->raydirx == 0) //length of ray from one x or y-side to next x or y-side
+		data->deltadistx = 1e30;
+	else
+		data->deltadistx = abs(1 / data->raydirx);
+	if (data->raydiry == 0) 
+		data->deltadisty = 1e30;
+	else
+		data->deltadisty = abs(1 / data->raydiry);
+	data->perpwalldist = 0;
+	data->stepx = 0; //what direction to step in x or y-direction (either +1 or -1)
+	data->stepy = 0;
+	data->hit = 0;
+	data->side = 0;
+	calculate_step_init_sidedist(data); //calculate step and initial sideDist
+}
+
+void	print_vertical_line(int x, t_data *data, int draw_start, int draw_end)
+{
+	int	y;
+	char	*dst;
+	char	*color;
+
+	color = 0xff00; 
+	y = 0;
+	if (data->side == 1)
+		color = 0xff11;
+	while (y < screenHeight)
+	{
+		if (y > draw_start && y < draw_end)
+		{
+			dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+			dst = color;
+		}
+		else
+			dst = 0x0;
+		y++;
+	}
+}
+
+void	calculate_wall_height(t_data *data, int x)
+{
+	int	line_height;
+	int	h;
+	int	draw_start;
+	int	draw_end;
+
+	h = screenHeight;
+	line_height = (int)(h / data->perpwalldist); //Calculate height of line to draw on screen
+	//calculate lowest and highest pixel to fill in current stripe
+	draw_start = -line_height / 2 + h / 2;
+	if (draw_start < 0)
+		draw_start = 0;
+	draw_end = line_height / 2 + h / 2;
+	if (draw_end >= h)
+		draw_end = h - 1;
+	print_vertical_line(x, data, draw_start, draw_end);
+}
+
+void	perform_dda(t_data *data, int x)
+{
+	while (data->hit == 0)
+	{
+		if (data->sidedistx < data->sidedisty)
+		{
+			data->sidedistx += data->deltadistx;
+			data->mapx += data->stepx;
+			data->side = 0;
+		}
+		else
+		{
+			data->sidedisty += data->deltadisty;
+			data->mapy += data->stepy;
+			data->side = 1;
+		}
+		if (data->worldmap[data->mapx][data->mapy] > 0)
+			data->hit = 1;
+	}
+	if (data->side == 0)
+		data->perpwalldist = (data->sidedistx - data->deltadistx);
+	else
+		data->perpwalldist = (data->sidedisty - data->deltadisty);
+	calculate_wall_height(data, x);
+
+}
+
+void	calculte_ray_pos(t_data *data)
+{
+	int	x;
+	int	w;
+
+	w = screenWidth;
+	x = 0;
+	while (x < w)
+	{
+		data->camerax = 2 * x / ((double)w - 1); //x-coordinate in camera space, calculate ray position and direction 3l+
+		data->raydirx = data->dirx + data->planex * data->camerax;
+		data->raydiry = data->diry + data->planey * data->camerax;
+		// calcul de la distance du prochain mur pour le rayon
+		ray_calcultion(data);
+		perform_dda(data, x);
+		x++;
+	}
+	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img, 0, 0);
+}
+
+int	done()
+{
+	return (1);
+}
+
+void img_init(t_data *data)
+{
+	data->img = mlx_new_image(data->mlx, screenWidth, screenHeight);
+	data->addr = mlx_get_data_addr(data->img, &data->bits_per_pixel,
+			&data->line_length, &data->endian);
+}
+
+int main()
+{
+	t_data	data;
+
+	data.mlx = mlx_init();
+	data.mlx_win = mlx_new_window(data.mlx, screenWidth, screenHeight, "Cube3D");
+	img_init(&data);
+	while (done())
+	{
+		calculate_ray_pos(&data);
+	}	
+}
